97a98fa5ef101dae9f6b8b36a0ddf5ec64681960
Rifatul Islam <shovonis09@gmail.com>
2020-02-20 14:47:15 -0600

    Initial Commit

---
+++ Assets/Scripts/BoatContoller.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class BoatContoller : MonoBehaviour
+{
+    
+    void FixedUpdate()
+    {
+        gameObject.transform.Rotate(Random.Range(-0.2f, 0.2f), 0, Random.Range(-0.2f, 0.2f));
+
+    }
+}
