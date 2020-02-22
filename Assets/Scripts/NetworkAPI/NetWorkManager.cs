757c65e25bf851a11535a6b1cac2ed9f5c3b9c9a
Rifatul Islam <shovonis09@gmail.com>
2020-02-21 19:04:15 -0600

    Tmp Commit

---
+++ Assets/Scripts/NetworkAPI/NetWorkManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace NetworkAPI
+{
+    public class NetWorkManager : MonoBehaviour
+    {
+        [SerializeField] private int _experimentLenght = 12;
+        [SerializeField] private int _dataCollectionFreq = 8; // See neulog api documentation
+        [SerializeField] private int _numberOfSamples = 10; // See neulog api documentation
+
+        private String _START_EXP_URL = "http://localhost:22002/NeuLogAPI?StartExperiment:[GSR],[1],[Pulse],[1]";
+        private String _STOP_EXP_URL = "http://localhost:22002/NeuLogAPI?StopExperiment";
+        private bool hasExperimentStarted;
+        public float endTimer = 300;
+        private Scene currentScene;
+        public static float timeToStop;
+        private float timeForNextExperiment;
+        private PythonServerAPI _pythonServerApi;
+        private NeuLogAPI _neuLogApi;
+
+        void Start()
+        {
+            _pythonServerApi = new PythonServerAPI();
+            _neuLogApi = new NeuLogAPI();
+            _pythonServerApi.Start();
+            hasExperimentStarted = false;
+
+            if (!hasExperimentStarted)
+            {
+                StartExperiment(_experimentLenght);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            timeToStop = timeToStop + Time.deltaTime;
+            timeForNextExperiment = timeForNextExperiment + Time.deltaTime;
+            bool loadNextScene = Input.GetKeyDown(KeyCode.N);
+
+            if (timeToStop >= endTimer && _pythonServerApi.hasDataProcessed)
+            {
+                LoadNextSceneAndStopExp(loadNextScene);
+            }
+            else if (timeForNextExperiment >= _experimentLenght && _pythonServerApi.hasDataProcessed)
+            {
+                _pythonServerApi?.Stop();
+                _pythonServerApi.Start();
+                StartExperiment(_experimentLenght);
+                timeForNextExperiment = 0;
+            }
+        }
+
+        private void LoadNextSceneAndStopExp(bool loadNextScene)
+        {
+            if (loadNextScene)
+            {
+                Logger.Log(LogLevel.INFO,
+                    "Next key pressed. Terminating initial static scene.");
+            }
+            else
+            {
+                Logger.Log(LogLevel.INFO, "Initial static Scene finished. Total runtime: " + endTimer + "s");
+            }
+
+            StopExperiment();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        private void StartExperiment(int length)
+        {
+            StartCoroutine(_neuLogApi.ClosePreviousExperiment(_STOP_EXP_URL)); // Close all previous started experiment
+            StartCoroutine(_neuLogApi.StartExperiment(_START_EXP_URL, _dataCollectionFreq.ToString(),
+                _numberOfSamples.ToString()));
+            
+            _pythonServerApi.SetMessageAndSend("" + length);
+            hasExperimentStarted = true;
+        }
+
+        private void StopExperiment()
+        {
+            StartCoroutine(_neuLogApi.StopExperiment(_STOP_EXP_URL));
+            _pythonServerApi.hasDataProcessed = false;
+            _pythonServerApi?.Stop();
+        }
+    }
+}
---
+++ Assets/Scripts/NetworkAPI/PythonServerAPI.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using AsyncIO;
+using NetMQ;
+using NetMQ.Sockets;
+using UnityEngine;
+
+namespace NetworkAPI
+{
+    public class PythonServerAPI : RunAbleThread
+    {
+        private bool isConnectionDone;
+        private RequestSocket client;
+        private String HAS_DATA_COLLECTED = "Done";
+
+        protected override void Run()
+        {
+            ForceDotNet.Force();
+            isConnectionDone = false;
+            using (client = new RequestSocket())
+            {
+                client.Connect("tcp://localhost:5555");
+                Debug.Log(_cmd);
+                SendTCPMessage(_cmd);
+                string message = null;
+                bool gotMessage = false;
+                
+                while (Running)
+                {
+                    gotMessage = client.TryReceiveFrameString(out message);
+                    if (gotMessage) break;
+                }
+
+                // If We got response that we got the data then invoke the stop Experiment
+                if (gotMessage)
+                {
+                    Debug.Log("Python Server finished collecting data: " + message);
+
+                    if (message.Contains(HAS_DATA_COLLECTED))
+                    {
+                        isConnectionDone = true;
+                    }
+                    else
+                    {
+                        Debug.LogError("ERROR! Python Server could not collect the sensor data!");
+                    }
+                }
+            }
+
+            NetMQConfig.Cleanup();
+        }
+
+        public void SendTCPMessage(String msg)
+        {
+            client?.SendFrame(msg);
+        }
+
+        public void SetCmd(String cmd)
+        {
+            _cmd = cmd;
+        }
+        
+        
+        public void SetMessageAndSend(String message)
+        {
+            isConnectionDone = false;
+            SetCmd(message);
+            // Start();
+        }
+
+        public bool hasDataProcessed
+        {
+            get => isConnectionDone;
+            set => isConnectionDone = value;
+        }
+    }
+}
