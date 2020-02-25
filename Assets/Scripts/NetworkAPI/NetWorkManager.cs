d410c358a188c7db9bff61cb107b171c9c46e812
Rifatul Islam <shovonis09@gmail.com>
2020-02-25 01:05:45 -0600

    Neulog API moved to python

--- Assets/Scripts/NetworkAPI/NetWorkManager.cs
+++ Assets/Scripts/NetworkAPI/NetWorkManager.cs
@@ -6,33 +6,24 @@ namespace NetworkAPI
 {
     public class NetWorkManager : MonoBehaviour
     {
-        [SerializeField] private int _dataCollectionFreq = 8; // See neulog api documentation
-        [SerializeField] private int _numberOfSamples = 10; // See neulog api documentation
         [SerializeField] private String _experimentName;
-
-        private String _START_EXP_URL = "http://localhost:22002/NeuLogAPI?StartExperiment:[GSR],[1],[Pulse],[1]";
-        private String _STOP_EXP_URL = "http://localhost:22002/NeuLogAPI?StopExperiment";
         private bool hasExperimentStarted;
         public float endTimer = 300;
         private Scene currentScene;
         public static float timeToStop;
         private float timeForNextExperiment;
         private PythonServerAPI _pythonServerApi;
-        private NeuLogAPI _neuLogApi;
-
         private String RESTING = "REST";
         private String SIMULATION = "SIM";
 
         void Start()
         {
             _pythonServerApi = new PythonServerAPI();
-            _neuLogApi = new NeuLogAPI();
             _pythonServerApi.Start();
-            
             hasExperimentStarted = false;
             if (!hasExperimentStarted)
             {
-                StartExperiment(_experimentName);
+                SendStartExperimentReq(_experimentName);
             }
         }
 
@@ -60,23 +51,18 @@ namespace NetworkAPI
                 Logger.Log(LogLevel.INFO, "Initial static Scene finished. Total runtime: " + endTimer + "s");
             }
 
-            StopExperiment();
+            CloseSeverConnection();
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
 
-        private void StartExperiment(String expName)
+        private void SendStartExperimentReq(String expName)
         {
-            StartCoroutine(_neuLogApi.ClosePreviousExperiment(_STOP_EXP_URL)); // Close all previous started experiment
-            StartCoroutine(_neuLogApi.StartExperiment(_START_EXP_URL, _dataCollectionFreq.ToString(),
-                _numberOfSamples.ToString()));
-            
             _pythonServerApi.SetMessageAndSend(expName);
             hasExperimentStarted = true;
         }
 
-        private void StopExperiment()
+        private void CloseSeverConnection()
         {
-            StartCoroutine(_neuLogApi.StopExperiment(_STOP_EXP_URL));
             _pythonServerApi.hasDataProcessed = false;
             _pythonServerApi?.Stop();
         }
